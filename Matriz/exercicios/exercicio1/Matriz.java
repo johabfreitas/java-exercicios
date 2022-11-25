2790fba76f5bf8e5154c0ae1406cba9563b334f6
Johab Freitas <freitasjohab@gmail.com>
2022-11-25 16:33:45 -0300

    Correção Exercicio 1 de matriz

--- Matriz/exercicios/exercicio1/Matriz.java
+++ Matriz/exercicios/exercicio1/Matriz.java
@@ -11,30 +11,46 @@ public class Matriz {
 		System.out.print("Enter column numbers: ");
 		int n = sc.nextInt();
 		int[][] mat = new int[m][n];
-		
+
 		System.out.println("Enter with numbers");
-		for(int i = 0; i < mat.length; i++) {
-			for(int j = 0; j < mat[i].length; j++) {
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
-		
+
 		System.out.print("Enter number: ");
 		int number = sc.nextInt();
-		int positionLine = 0;
-		int positionColumn = 0;
-		for(int i = 0; i < mat.length; i++) {
-			for(int j = 0; j < mat[i].length; j++) {
-				if(mat[i][j] == number) {
-					positionLine = i;
-					positionColumn = j;			
-					System.out.println("Position " + positionLine + ", " + positionColumn + ": ");
-				}
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == number) {
+					
+					System.out.println("Position " + i + "," + j + ":");
+					
+					if (j > 0) {
+						System.out.println("Left: " + mat[i][j-1]);
+					}
+					if (i > 0) {
+						System.out.println("Up: " + mat[i-1][j]);
+					}
+					if (j < mat[i].length-1) {
+						System.out.println("Right: " + mat[i][j+1]);
+					}
+					if (i < mat.length-1) {
+						System.out.println("Down: " + mat[i+1][j]);
+					}
+
+					/*
+					System.out.println("Left: " + mat[positionLine][positionColumn - 1]);
+					System.out.println("Right: " + mat[positionLine][positionColumn + 1]);
+					System.out.println("Up: " + mat[positionLine - 1][positionColumn]);
+					System.out.println("Down: " + mat[positionLine + 1][positionColumn]);
+					*/
+					
+				} 
 			}
 		}
-		
-		
-		
+
 		sc.close();
 
 	}
