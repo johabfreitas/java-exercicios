e06580b019bb1f77214049383be784626af1c099
Johab <freitasjohab@gmail.com>
2022-11-26 00:48:54 -0300

    Date Time

--- DataHora/src/application/Program01.java
+++ DataHora/src/application/Program01.java
@@ -3,6 +3,7 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program01 {
@@ -10,8 +11,12 @@ public class Program01 {
 	public static void main(String[] args) {
 		
 		// https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/time/format/DateTimeFormatter.html
+		
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());// Pega date time local
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 		
 		LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 = LocalDateTime.now();
@@ -32,9 +37,16 @@ public class Program01 {
 		System.out.println("d01 = " + d01);
 		System.out.println("d02 = " + d02);
 		System.out.println("d03 = " + d03);
-		System.out.println("d04 = " + d04);
+		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d04 = " + fmt1.format(d04));
+		System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 		System.out.println("d05 = " + d05);
+		System.out.println("d05 = " + d05.format(fmt1));
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d05 = " + d05.format(fmt4));
 		System.out.println("d06 = " + d06);
+		System.out.println("d06 = " + fmt3.format(d06));
+		System.out.println("d06 = " + fmt5.format(d06));
 		System.out.println("d07 = " + d07);
 		System.out.println("d08 = " + d08);
 		System.out.println("d09 = " + d09);
