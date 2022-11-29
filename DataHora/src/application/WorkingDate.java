21bf7e389fe509aa33f2025e552b2f862ed9d7a6
Johab Freitas <freitasjohab@gmail.com>
2022-11-29 10:34:52 -0300

    Trabalhando com Datas

--- DataHora/src/application/WorkingDate.java
+++ DataHora/src/application/WorkingDate.java
@@ -18,8 +18,8 @@ public class WorkingDate {
 		Date y1 = sdf1.parse("25/06/2018");
 		Date y2 = sdf2.parse("25/06/2018 15:42:07");
 		
-		System.out.println(y1);
-		System.out.println(y2);
+		System.out.println(sdf1.format(y1));
+		System.out.println(sdf2.format(y2));
 		System.out.println("d01 = " + d01.format(fmt1));
 
 	}
