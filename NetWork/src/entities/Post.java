f919d445e4cfc42961763b348822fc8bd632ed8c
Johab Freitas <freitasjohab@gmail.com>
2022-11-30 14:57:48 -0300

    Update Project NetWork

--- NetWork/src/entities/Post.java
+++ NetWork/src/entities/Post.java
@@ -1,11 +1,14 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
 
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
 	private Date moment;
 	private String title;
 	private String content;
@@ -65,5 +68,20 @@ public class Post {
 	
 	public void removeComment(Comment comment) {
 		comments.remove(comment);
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes);		
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append(content + "\n");
+		sb.append("Comments:\n");
+		for (Comment c : comments) {
+			sb.append(c.getText() + "\n");
+		}
+		
+		return sb.toString();
 	}
 }
