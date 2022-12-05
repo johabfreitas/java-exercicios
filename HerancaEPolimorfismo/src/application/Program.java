804b124689517fcd9d0d20274f69b16030a9888a
Johab <freitasjohab@gmail.com>
2022-12-05 20:23:45 -0300

    Abstração

--- HerancaEPolimorfismo/src/application/Program.java
+++ HerancaEPolimorfismo/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import entities.Account;
@@ -11,18 +13,28 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
+		List<Account> list = new ArrayList<>();
 		
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
+		list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1004, "Bob", 300.00, 0.01));
+		list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 		
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
-
+		double sum = 0.0;
+		for(Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for(Account acc : list) {
+			acc.deposit(10.0);
+		}
+		
+		for(Account acc : list) {
+			System.out.printf("Update balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());;
+		}
+		
 	}
 
 }
